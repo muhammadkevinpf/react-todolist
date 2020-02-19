cb15bc9087bcced25929118d579f33558230851e
muhammadkevinpf <43286448+muhammadkevinpf@users.noreply.github.com>
2020-02-19 18:31:12 +0700

    todo state

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,20 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Todo from "./Todo";
-const Todos = () => {
-  const todos = [
-    {
-      text: "EDAAAN"
-    },
-    {
-      text: "EDAAAN"
-    },
-    {
-      text: "EDAAAN"
-    },
-    {
-      text: "EDAAAN"
-    }
-  ];
+const Todos = ({ todos }) => {
   return (
     <section className="todos">
       {todos.map(todo => (
@@ -22,6 +9,14 @@ const Todos = () => {
       ))}
     </section>
   );
+};
+
+Todos.prototype = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string
+    })
+  )
 };
 
 export default Todos;
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Paper from "../components/Paper";
 import Header from "../components/Header";
 import TodoForm from "../components/TodoForm";
 import Todos from "../components/Todos";
 export default function TodoList() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Belajar React"
+    },
+    {
+      text: "belajar state"
+    }
+  ]);
+
   return (
     <Paper>
       <Header />
       <TodoForm />
-      <Todos />
+      <Todos todos={todos} />
     </Paper>
   );
 }
